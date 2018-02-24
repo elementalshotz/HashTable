b051e52a9dc609b55935e8788f7595d2ce70aa4d
Dennis Lund <denniz_lund@hotmail.com>
2018-02-24 09:22:18 +0100

    Tuple

--- HashTable/Key.cs
+++ HashTable/Key.cs
@@ -6,8 +6,20 @@ using System.Threading.Tasks;
 
 namespace HashTable
 {
-    class Key 
+    class Key
     {
-       
+        private double Long;
+        private double Lat;
+
+        public Key(double longitude, double lat)
+        {
+            this.Long = longitude;
+            this.Lat = lat;
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(Long, Lat).GetHashCode();
+        }
     }
 }
