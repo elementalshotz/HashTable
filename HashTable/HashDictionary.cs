d04e81734e90a36ea1e9cf91c1b24ca78cba320a
Dennis Lund <denniz_lund@hotmail.com>
2018-02-25 15:05:16 +0100

    Changed HashDictionary

--- HashTable/HashDictionary.cs
+++ HashTable/HashDictionary.cs
@@ -7,80 +7,81 @@ using System.Threading.Tasks;
 
 namespace HashTable
 {
-    class HashDictionary<K,V> where K : Key<K>, IDictionary<K, V>
+    class HashDictionary<K, V> : IDictionary<K, V>
+
     {
-        private HashDictionary<K, V> hashDictionary = new HashDictionary<K, V>();
+    private HashDictionary<K, V> hashDictionary = new HashDictionary<K, V>();
 
-        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+    public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+    {
+        return hashDictionary.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return hashDictionary.GetEnumerator();
+    }
+
+    public void Add(KeyValuePair<K, V> item)
+    {
+        hashDictionary.Add(item);
+    }
+
+    public void Clear()
+    {
+        hashDictionary.Clear();
+    }
+
+    public bool Contains(KeyValuePair<K, V> item)
+    {
+        return hashDictionary.Contains(item);
+    }
+
+    public void CopyTo(KeyValuePair<K, V>[] array, int arrayIndex)
+    {
+        foreach (var item in hashDictionary)
         {
-            return hashDictionary.GetEnumerator();
+            array[arrayIndex] = item;
+            arrayIndex++;
         }
 
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return (IEnumerator) hashDictionary.GetEnumerator();
-        }
+    }
 
-        public void Add(KeyValuePair<K, V> item)
-        {
-            hashDictionary.Add(item);
-        }
+    public bool Remove(KeyValuePair<K, V> item)
+    {
+        return hashDictionary.Remove(item);
+    }
 
-        public void Clear()
-        {
-            hashDictionary.Clear();
-        }
+    public int Count { get; }
+    public bool IsReadOnly { get; }
 
-        public bool Contains(KeyValuePair<K, V> item)
-        {
-            return hashDictionary.Contains(item);
-        }
+    public bool ContainsKey(K key)
+    {
+        return hashDictionary.ContainsKey(key);
+    }
 
-        public void CopyTo(KeyValuePair<K, V>[] array, int arrayIndex)
-        {
-            foreach (var item in hashDictionary)
-            {
-                array[arrayIndex] = item;
-                arrayIndex++;
-            }
+    public void Add(K key, V value)
+    {
+        hashDictionary.Add(key, value);
+    }
 
-        }
+    public bool Remove(K key)
+    {
+        return hashDictionary.Remove(key);
+    }
 
-        public bool Remove(KeyValuePair<K, V> item)
-        {
-            return hashDictionary.Remove(item);
-        }
+    public bool TryGetValue(K key, out V value)
+    {
+        return hashDictionary.TryGetValue(key, out value);
+    }
 
-        public int Count { get; }
-        public bool IsReadOnly { get; }
+    public V this[K key]
+    {
+        get => hashDictionary[key];
+        set => hashDictionary[key] = value;
+    }
 
-        public bool ContainsKey(K key)
-        {
-            return hashDictionary.ContainsKey(key);
-        }
-
-        public void Add(K key, V value)
-        {
-            hashDictionary.Add(key,value);
-        }
-
-        public bool Remove(K key)
-        {
-            return hashDictionary.Remove(key);
-        }
-
-        public bool TryGetValue(K key, out V value)
-        {
-            return hashDictionary.TryGetValue(key, out value);
-        }
-
-        public V this[K key]
-        {
-            get => hashDictionary[key];
-            set => hashDictionary[key] = value;
-        }
-
-        public ICollection<K> Keys { get; }
-        public ICollection<V> Values { get; }
+    public ICollection<K> Keys { get; }
+    public ICollection<V> Values { get; }
     }
 }
